80f1b5f35ff806766c88fa1cd716b8c9c0c61d29
Nayely Rodarte <nayelyrodarte@gmail.com>
2024-06-07 15:47:27 -0400

    Update header component

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,36 +1,58 @@
 <script lang="ts">
+  import PlaceholderImage from '../assets/img/placeholder.png';
+
   export let title: string = '';
   export let caption: string = '';
   export let description: string = '';
+  // export let image: 'valle' | 'mezcales' = 'valle';
 </script>
 
 <header>
-  <p class="caption">{caption}</p>
-  <h1>{title}</h1>
-  <p class="header-description">
-    {description}
-  </p>
+  <img src={PlaceholderImage} alt="" />
+  <section class="header-content">
+    <p class="caption">{caption}</p>
+    <h1>{title}</h1>
+    <p class="header-description">
+      {description}
+    </p>
+  </section>
 </header>
 
 <style>
   header {
-    display: flex;
-    margin: auto;
-    max-width: var(--max-width);
+    width: 100%;
+  }
 
+  header,
+  .header-content {
+    display: flex;
     justify-content: center;
     align-items: center;
+    flex-direction: column;
+    gap: 1.5rem;
 
+    margin: auto;
     text-align: center;
   }
 
-  header {
-    flex-direction: column;
-    gap: 1.5rem;
+  .header-content {
+    max-width: var(--max-width);
+
     padding: var(--spacing-xl);
   }
 
   .header-description {
     max-width: 110ch;
+  }
+
+  img {
+    max-height: 36rem;
+    width: 100%;
+    padding: 0.4rem;
+
+    box-sizing: border-box;
+    border-radius: 10px;
+
+    object-fit: cover;
   }
 </style>
