1ae351cb3d873c6a9172ea15c1c6673b753f4439
Nayely Rodarte <nayelyrodarte@gmail.com>
2024-06-12 11:25:01 -0400

    Create the horizontal card component

--- src/components/mezcales/HorizontalCard.svelte
+++ src/components/mezcales/HorizontalCard.svelte
@@ -4,14 +4,48 @@
   export let caption: string = 'Hecho en México';
   export let title: string = '';
   export let subtitle: string = '';
-  export let description: string = '';
+  export let bgColor: 'salmon' | 'green' | 'red' = 'salmon';
+
+  let bgColors = {
+    salmon: 'var(--salmon)',
+    green: 'var(--green-500)',
+    red: 'var(--red-300)',
+  };
 </script>
 
-<article>
-  <slot class="image"></slot>
-  <p>{caption}</p>
-  <h1>{title}</h1>
-  <p>{subtitle}</p>
-  <Jabali />
-  <p>{description}</p>
+<article class="horizontal-card" style="background-color: {bgColors[bgColor]}">
+  <div class="image"><slot name="image"></slot></div>
+  <div class="horizontal-card-inner">
+    <p>{caption}</p>
+    <h1>{title}</h1>
+    <p>{subtitle}</p>
+    <Jabali />
+    <slot name="description"></slot>
+  </div>
 </article>
+
+<style>
+  .horizontal-card {
+    display: flex;
+    max-width: var(--max-width);
+    height: 50rem;
+
+    border-radius: 0 20px 20px 0;
+    border: solid 1px var(--black-700);
+  }
+
+  .horizontal-card-inner {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    text-align: center;
+    margin: auto;
+    padding: var(--spacing-lg);
+  }
+
+  .image {
+    width: 50%;
+  }
+</style>
