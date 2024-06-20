5cdb709cf74853bf47e1db21e36e47e53ce55bdf
Nayely Rodarte <nayelyrodarte@gmail.com>
2024-06-20 16:45:32 -0400

    Add text accents

--- src/routes/origenes/+page.svelte
+++ src/routes/origenes/+page.svelte
@@ -11,16 +11,19 @@
   <ImageComponent src={HeaderImg} slot="image" />
   <section slot="custom-description" class="custom-description">
     <p>
-      SAGRADO PARA NUESTROS ANCESTROS, EL AGAVE SALMIANA PERDURA EN EL DESIERTO
-      COMO LAS LEYENDAS CONTADAS EN SU NOMBRE.
+      SAGRADO PARA NUESTROS ANCESTROS, <span class="accent-text bold-text"
+        >AGAVE SALMIANA</span
+      > PERDURA EN EL DESIERTO COMO LAS LEYENDAS CONTADAS EN SU NOMBRE.
     </p>
     <p>
-      CON SUS PENCAS GRUESAS Y CARNOSAS, QUE NACEN DEL TALLO Y REMATAN EN UNA
-      ESPINA FUERTE Y OSCURA.
+      CON SUS <span class="bold-text">PENCAS GRUESAS Y CARNOSAS</span>, QUE
+      NACEN DEL TALLO Y REMATAN EN UNA ESPINA FUERTE Y OSCURA.
     </p>
     <p>
-      PERFUMES FLORALES QUE TRASCIENDEN EN CUALQUIER LUGAR, FORMAN EL CARACTER Y
-      ESENCIA DE NUESTRO MEZCAL SALMERO.
+      PERFUMES FLORALES QUE TRASCIENDEN EN CUALQUIER LUGAR, FORMAN <span
+        class="bold-text accent-text">CARACTER Y ESENCIA</span
+      >
+      EL DE NUESTRO <span class="bold-text">MEZCAL SALMERO</span>.
     </p>
   </section>
 </Header>
@@ -32,13 +35,15 @@
     class="nuestra-tierra featured-description"
   >
     <p>
-      EL LÍMITE DE MESOAMÉRICA Y ARIDOAMÉRICA, DONDE LOS BRAVOS SE JUNTAN CON
-      LOS CIVILIZADOS, DONDE EL DESIERTO IMPONE SU LEY Y DEJA VIVIR SOLO A LOS
-      MÁS RESILIENTES.
+      EL LÍMITE DE <span class="bold-text">MESOAMÉRICA Y ARIDOAMÉRICA</span>,
+      DONDE LOS BRAVOS SE JUNTAN CON LOS CIVILIZADOS, DONDE EL DESIERTO IMPONE
+      SU LEY Y DEJA VIVIR SOLO A LOS MÁS RESILIENTES.
     </p>
     <p>
-      ORgULLO POTOSINO QUE RINDE HOMENAJE A LA UNIÓN DE DOS MUNDOS COMO EL MISMO
-      SAN LUIS. SALMERO ES LA REPRESENTACIÓN COMPLETA DEL ALTIPLANO.
+      <span class="bold-text">ORgULLO POTOSINO</span> QUE RINDE HOMENAJE A LA
+      UNIÓN DE DOS MUNDOS COMO EL MISMO SAN LUIS.
+      <span class="bold-text accent-text">SALMERO</span> ES LA REPRESENTACIÓN
+      COMPLETA DEL <span class="bold-text">ALTIPLANO</span>.
     </p>
   </section>
 </FeaturedImage>
@@ -50,22 +55,26 @@
     class="nuestra-historia featured-description"
   >
     <p>
-      Fue en el Siglo XVII que los Monjes Carmelitos Descalzos llegaron al
-      Altiplano Potosino, y ahí, introdujeron el primer alambique para procesar
-      Mezcal en México, donde no solamente fue el primer Mezcal de Mexico, si no
-      probablemente el primer destilado en todo el continente americano. De esta
-      forma, San Luis Potosí se convirtió hasta las épocas de la revolución en
-      el productor mas grande de destilados en el país y en el continente. Como
-      toda buena historia, sufre su tragedia después de la revolución, donde mas
-      de 200 fabricas se vieron forzadas a cerrar sus puertas ya que la reforma
-      agraria no fue compatible con el modelo económico que terminaba, el de las
-      Haciendas.
+      Fue en el <span class="bold-text">Siglo XVII</span> que los Monjes
+      Carmelitos Descalzos llegaron al
+      <span class="bold-text accent-text">Altiplano Potosino</span>, y ahí,
+      introdujeron el primer alambique para procesar Mezcal en México, donde no
+      solamente fue el primer Mezcal de Mexico, si no probablemente el primer
+      destilado en todo el continente americano. De esta forma,
+      <span class="bold-text">San Luis Potosí se convirtió</span> hasta las
+      épocas de la revolución
+      <span class="bold-text"
+        >en el productor mas grande de destilados en el país y en el continente</span
+      >. Como toda buena historia, sufre su tragedia después de la revolución,
+      donde mas de 200 fabricas se vieron forzadas a cerrar sus puertas ya que
+      la reforma agraria no fue compatible con el modelo económico que
+      terminaba, el de las Haciendas.
     </p>
     <p>
-      Ahora, 100 años después, Mezcal Salmero está presente para recordar
-      Nuestra Historia y no dejarnos olvidar de dónde viene este Mezcal tan
-      tradicional no solo en su elaboración, sino en la cultura de nuestro
-      Estado y nuestro Pais.
+      Ahora, 100 años después, <span class="bold-text"
+        >Mezcal Salmero está presente para recordar Nuestra Historia</span
+      > y no dejarnos olvidar de dónde viene este Mezcal tan tradicional no solo
+      en su elaboración, sino en la cultura de nuestro Estado y nuestro Pais.
     </p>
   </section>
 </FeaturedImage>
@@ -98,6 +107,9 @@
   }
 
   .nuestra-historia.featured-description {
+    display: flex;
+    flex-direction: column;
     max-width: 150ch;
+    gap: var(--spacing-lg);
   }
 </style>
