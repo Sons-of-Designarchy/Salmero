db1366543fb33b3f23377a390c2ac5ccc1576757
Nayely Rodarte <nayelyrodarte@gmail.com>
2024-06-24 11:43:29 -0400

    add steps sections to proceso page

--- src/routes/proceso/+page.svelte
+++ src/routes/proceso/+page.svelte
@@ -1,7 +1,7 @@
 <script>
   import Header from '../../components/Header.svelte';
   import ImageComponent from '../../utils/ImageComponent.svelte';
-  
+
   import ProcesoImg from '../../assets/img/Proceso_header.png';
   import Jabali from '../../assets/svg/Jabali.svelte';
   import Proceso_1 from '../../assets/img/Proceso_1.png';
@@ -33,6 +33,7 @@
   <article>
     <div class="proceso-steps-img"><Agave /></div>
     <p>1</p>
+    <h3>Selección</h3>
     <p>
       el Agave Salmiana madura en 10 a 12 años. Luego de un año más de espera,
       los campesinos cosechan el agave para llevarlo al horno. Este proceso
@@ -41,8 +42,9 @@
     </p>
   </article>
   <article>
-   <div class="proceso-steps-img"> <HotStones /></div>
-   <p>2</p>
+    <div class="proceso-steps-img"><HotStones /></div>
+    <p>2</p>
+    <h3>Cocción</h3>
     <p>
       el Agave Salmiana madura en 10 a 12 años. Luego de un año más de espera,
       los campesinos cosechan el agave para llevarlo al horno. Este proceso
@@ -50,43 +52,95 @@
       fabricación artesanal del mezcal.
     </p>
   </article>
+  <article>
+    <div class="proceso-steps-img"><HotStones /></div>
+    <p>3</p>
+    <h3>Fermentación</h3>
+    <p>
+      Nuestro proceso 100% Natural se ve determinado por las fluctuaciones de
+      temperatura en el Altiplano y por consecuencia la fermentación se logra
+      desde 2 días en los veranos, hasta 5 días en los inviernos.
+    </p>
+  </article>
+  <article>
+    <div class="proceso-steps-img"><HotStones /></div>
+    <p>4</p>
+    <h3>Destilación</h3>
+    <p>
+      La destilación se realiza en Alambiques de Cobre y Alambiques de
+      Platillos. Con este método se logra la separación de alcoholes óptimos
+      para crear el sabor que hace único a Mezcal Salmero en sus tres
+      presentaciones.
+    </p>
+  </article>
 </section>
 
 <style>
+  .proceso-intro {
+    display: grid;
 
- .proceso-intro {
-  display: flex;
-  align-content: center;
- }
+    max-width: var(--max-width);
+    margin: auto;
+    text-align: start;
+
+    gap: 1rem;
+
+    @media only screen and (min-width: 48em) {
+      grid-auto-flow: column;
+      grid-template-columns: 1fr 30% 30%;
+      justify-items: center;
+
+      align-items: center;
+    }
+  }
+
+  .proceso-intro p {
+    max-width: 80ch;
+  }
+
+  .proceso-intro h1 {
+    grid-row: 2/3;
+
+    @media only screen and (min-width: 48em) {
+      grid-row: inherit;
+    }
+  }
 
   .proceso-images-grid {
     display: grid;
-    grid-template-columns: repeat(3, 1fr);
+    grid-template-columns: repeat(1, 100%);
+    @media only screen and (min-width: 48em) {
+      grid-template-columns: repeat(3, 1fr);
+    }
   }
 
   .proceso-steps {
     display: flex;
+    row-gap: 4rem;
     flex-wrap: wrap;
+    justify-content: center;
 
-    max-width: var(--max-width);
     margin: auto;
-
-    padding: calc(var(--spacing-xl) * 2);
-    gap: calc(var(--spacing-xl) * 2);
+    padding: calc(var(--spacing-xl) * 2) 0;
   }
 
   .proceso-steps article {
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
+    justify-content: start;
+    gap: 1rem;
 
-    width: 20rem;
+    width: 25rem;
 
     text-align: center;
   }
 
+  .proceso-steps article p {
+    max-width: 35ch;
+  }
+
   .proceso-steps-img {
-   width: 8rem;
+    width: 8rem;
   }
 </style>
