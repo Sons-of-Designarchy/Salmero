7c0831ccc9ab56552c23974a999ee54394898db0
Daniel Pliego <danielpliego@gmail.com>
2024-08-13 09:36:54 -0600

    fix styles

--- src/components/LocaleSwitcher.svelte
+++ src/components/LocaleSwitcher.svelte
@@ -1,10 +1,55 @@
 <script lang="ts">
-  import { locale } from 'svelte-i18n';
+  import { locale } from "svelte-i18n";
+  import { get } from "svelte/store";
+
+  let currentLocale = get(locale);
+
+  $: {
+    // Subscribe to the locale store to keep currentLocale updated
+    const unsubscribe = locale.subscribe((value) => {
+      currentLocale = value;
+    });
+  }
 
   function changeLocale(newLocale: string) {
     locale.set(newLocale);
   }
 </script>
 
-<button on:click={() => changeLocale('en')}>En</button>
-<button on:click={() => changeLocale('es')}>Es</button>
+<div class="locale-switcher">
+  <button
+    class:active={currentLocale === "en"}
+    on:click={() => changeLocale("en")}
+  >
+    En
+  </button>
+  |
+  <button
+    class:active={currentLocale === "es"}
+    on:click={() => changeLocale("es")}
+  >
+    Es
+  </button>
+</div>
+
+<style>
+  .locale-switcher {
+    background: var(--salmon);
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    padding: 1rem;
+    color: #666;
+  }
+
+  .locale-switcher button {
+    padding: 0.25em 0.5em;
+    border: none;
+    cursor: pointer;
+  }
+
+  .locale-switcher button.active {
+    font-weight: bold;
+    text-decoration: underline;
+  }
+</style>
